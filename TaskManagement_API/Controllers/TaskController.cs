5e2b76f8a7c4131ed32fbcf28df2597230674756
Jomon Joseph <josephcreed10@gmail.com>
2026-04-13 08:11:00 +1000

    Controller methods for tasks

---
+++ TaskManagement_API/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagement_API.Controllers
+{
+    [ApiController]
+    [Route("tasks")]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetTasks()
+        {
+            return new string[] { "Task1", "Task2" };
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<string> GetTaskById([FromRoute] int id)
+        {
+            return $"Task{id}";
+        }
+
+        [HttpPost]
+        public ActionResult<string> CreateTask(string task)
+        {
+            return $"Created task: {task}";
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<string> UpdateTask([FromRoute] int id, [FromBody] string task)
+        {
+            return $"Updated task {id} with new value: {task}";
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<string> DeleteTask([FromRoute] int id)
+        {
+            return $"Deleted task {id}";
+        }
+    }
+}
