a335c1393f593bdc387a91d83f89eee4e73f8234
Jomon Joseph <josephcreed10@gmail.com>
2026-04-13 11:18:24 +1000

    TaskItem Entity Updation

--- TaskManagement_API/Entities/TaskItem.cs
+++ TaskManagement_API/Entities/TaskItem.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Server.HttpSys;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
+﻿using System.ComponentModel.DataAnnotations;
 namespace TaskManagement_API.Entities
 {
     public class TaskItem
@@ -9,15 +6,15 @@ namespace TaskManagement_API.Entities
         [Key]
         public int Id { get; set; }
         [Required] 
-        public required string  Title { get; set; }
+        public required string Title { get; set; }
         public string? Description { get; set; }
         [Required]
         public required string Status { get; set; }
         [Required]
         public required string Priority { get; set; }
-        public DateTime DueDate { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
         
     }
 }
