5c85284b61b074a97192e04ad978ee3f07641d80
Jomon Joseph <josephcreed10@gmail.com>
2026-04-14 09:52:38 +1000

    Registered Services for TaskService and TaskRepository

--- TaskManagement_API/Program.cs
+++ TaskManagement_API/Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using TaskManagement_API.Entities;
+using TaskManagement_API.IServices;
 using TaskManagement_API.Profiles;
+using TaskManagement_API.Repositories;
+using TaskManagement_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(config => config.AddProfile<TaskProfile>());
