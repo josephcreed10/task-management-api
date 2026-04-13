efb67a2db22a7876581d52c4c3fa290d50d5c7c8
Jomon Joseph <josephcreed10@gmail.com>
2026-04-13 13:19:15 +1000

    DTOs for data transfer

---
+++ TaskManagement_API/Models/DTOs/TaskCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TaskManagement_API.Models.DTOs
+{
+    public class TaskCreateDTO
+    {
+        [Required]
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public required string Status { get; set; }
+        [Required]
+        public required string Priority { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
---
+++ TaskManagement_API/Models/DTOs/TaskUpdateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagement_API.Models.DTOs
+{
+    public class TaskUpdateDTO
+    {
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public required string Status { get; set; }
+        public required string Priority { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
