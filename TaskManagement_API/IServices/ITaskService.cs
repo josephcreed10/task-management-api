e3517fc2911cf471878b4fde67276b13f6641e66
Jomon Joseph <josephcreed10@gmail.com>
2026-04-14 07:02:43 +1000

    Added TaskService Interface

---
+++ TaskManagement_API/IServices/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using TaskManagement_API.Models.DTOs;
+
+namespace TaskManagement_API.IServices
+{
+    public interface ITaskService
+    {
+        public Task<List<TaskReadDTO>> GetAllTasksAsync();
+        public Task<TaskReadDTO> GetTaskByIdAsync(int id);
+        public Task<TaskReadDTO> CreateTaskAsync(TaskCreateDTO taskCreateDTO);
+        public Task<TaskReadDTO> UpdateTaskByIdAsync(int id, TaskUpdateDTO taskUpdateDTO);
+        public Task<bool> DeleteTaskByIdAsync(int id);
+
+    }
+}
