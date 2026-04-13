00e49bc0df0eb0c8f8e599ec335390e6375d0b3d
Jomon Joseph <josephcreed10@gmail.com>
2026-04-14 07:15:48 +1000

    Interface updated version

--- TaskManagement_API/IServices/ITaskService.cs
+++ TaskManagement_API/IServices/ITaskService.cs
@@ -4,10 +4,10 @@ namespace TaskManagement_API.IServices
 {
     public interface ITaskService
     {
-        public Task<List<TaskReadDTO>> GetAllTasksAsync();
-        public Task<TaskReadDTO> GetTaskByIdAsync(int id);
-        public Task<TaskReadDTO> CreateTaskAsync(TaskCreateDTO taskCreateDTO);
-        public Task<TaskReadDTO> UpdateTaskByIdAsync(int id, TaskUpdateDTO taskUpdateDTO);
+        public Task<List<TaskReadDTO>>GetAllTasksAsync();
+        public Task<TaskReadDTO?> GetTaskByIdAsync(int id);
+        public Task<TaskReadDTO>CreateTaskAsync(TaskCreateDTO taskCreateDTO);
+        public Task<TaskReadDTO?> UpdateTaskByIdAsync(int id, TaskUpdateDTO taskUpdateDTO);
         public Task<bool> DeleteTaskByIdAsync(int id);
 
     }
