685b4a2e535dfb45fb9fbb75b94eaca8e90b55d8
Jomon Joseph <josephcreed10@gmail.com>
2026-04-13 13:53:35 +1000

    Added AutoMapper profile for DTOs

---
+++ TaskManagement_API/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TaskManagement_API.Entities;
+using TaskManagement_API.Models.DTOs;
+namespace TaskManagement_API.Profiles
+{
+    public class TaskProfile: Profile
+    {
+        public TaskProfile()
+        {
+            CreateMap<TaskCreateDTO, TaskItem>();
+            CreateMap<TaskUpdateDTO, TaskItem>();
+            CreateMap<TaskReadDTO, TaskItem>().ReverseMap();
+        }
+    }
+}
--- TaskManagement_API/Program.cs
+++ TaskManagement_API/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using TaskManagement_API.Entities;
+using TaskManagement_API.Profiles;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(config => config.AddProfile<TaskProfile>());
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 var app = builder.Build();
